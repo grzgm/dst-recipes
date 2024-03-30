fe75c3473234481e5e9d05d2ec37f3a70a809c20
grzgm <g.malisz@student.fontys.nl>
2024-03-30 13:30:58 +0100

    Updates

---
+++ precache-manifest.58a3adf284846120b32a7c685dab597f.js
@@ -0,0 +1,598 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be902f1efcce33b9fea625bd6ad06ab",
+    "url": "/dst-recipes/index.html"
+  },
+  {
+    "revision": "26ae94810a208ddab673",
+    "url": "/dst-recipes/static/css/2.ddb908ec.chunk.css"
+  },
+  {
+    "revision": "1914cad4bae3f0bf2b53",
+    "url": "/dst-recipes/static/css/main.53f14a89.chunk.css"
+  },
+  {
+    "revision": "26ae94810a208ddab673",
+    "url": "/dst-recipes/static/js/2.3d7751d2.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/dst-recipes/static/js/2.3d7751d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1914cad4bae3f0bf2b53",
+    "url": "/dst-recipes/static/js/main.78f15696.chunk.js"
+  },
+  {
+    "revision": "24b32268c3404cd6fa31",
+    "url": "/dst-recipes/static/js/runtime-main.b16a9fb8.js"
+  },
+  {
+    "revision": "93feaf8f8844448208edda8972f98bc7",
+    "url": "/dst-recipes/static/media/1.93feaf8f.jpg"
+  },
+  {
+    "revision": "2a45867de676f9908ab715293bead58c",
+    "url": "/dst-recipes/static/media/2.2a45867d.png"
+  },
+  {
+    "revision": "e606c26ed178840aec714c6840c33f66",
+    "url": "/dst-recipes/static/media/3.e606c26e.png"
+  },
+  {
+    "revision": "5cc95349fb1676261897b2635bb4f0fd",
+    "url": "/dst-recipes/static/media/4.5cc95349.png"
+  },
+  {
+    "revision": "4c5aefe2b06679f12eae8012c844f2ac",
+    "url": "/dst-recipes/static/media/5.4c5aefe2.jpg"
+  },
+  {
+    "revision": "8f7f480c8fcd3948a318e8de23a8a818",
+    "url": "/dst-recipes/static/media/Blue_Cap.8f7f480c.webp"
+  },
+  {
+    "revision": "26d074e2c9ae12ef3243bc658bd3f970",
+    "url": "/dst-recipes/static/media/Braised_Eggplant.26d074e2.webp"
+  },
+  {
+    "revision": "01a1d638c5c66310b6c11c2e742be286",
+    "url": "/dst-recipes/static/media/Cactus_Flesh.01a1d638.webp"
+  },
+  {
+    "revision": "e16372c94167b1f2e891b7d1f80bba00",
+    "url": "/dst-recipes/static/media/Cactus_Flower.e16372c9.webp"
+  },
+  {
+    "revision": "0dc4f95d13ca44a45fd0921cb606a506",
+    "url": "/dst-recipes/static/media/Carrot.0dc4f95d.webp"
+  },
+  {
+    "revision": "333662de47cb78f5ee6d649d661f56ce",
+    "url": "/dst-recipes/static/media/Cooked_Blue_Cap.333662de.webp"
+  },
+  {
+    "revision": "b0f38c1cae81b6f9c645f4931032a6f6",
+    "url": "/dst-recipes/static/media/Cooked_Cactus_Flesh.b0f38c1c.webp"
+  },
+  {
+    "revision": "06ba966fe90124231068548c19e33c35",
+    "url": "/dst-recipes/static/media/Cooked_Green_Cap.06ba966f.webp"
+  },
+  {
+    "revision": "960cb94afcde253bfa4af3c30e16b0b3",
+    "url": "/dst-recipes/static/media/Cooked_Kelp_Fronds.960cb94a.webp"
+  },
+  {
+    "revision": "59b5049b61e5993ee74d2cdf71d88f0e",
+    "url": "/dst-recipes/static/media/Cooked_Mandrake.59b5049b.webp"
+  },
+  {
+    "revision": "0515999b9ed6bb0daadb6585f8c7e138",
+    "url": "/dst-recipes/static/media/Cooked_Red_Cap.0515999b.webp"
+  },
+  {
+    "revision": "e0430d9d1269feef8c659f43c4783e40",
+    "url": "/dst-recipes/static/media/Cooked_Stone_Fruit.e0430d9d.webp"
+  },
+  {
+    "revision": "058a640ef3aff87f6aefec5aa2bb48d7",
+    "url": "/dst-recipes/static/media/Corn.058a640e.webp"
+  },
+  {
+    "revision": "cf17054bb65bddaa1f54a8f9e580996e",
+    "url": "/dst-recipes/static/media/Dried_Kelp_Fronds.cf17054b.webp"
+  },
+  {
+    "revision": "9374fe09af3834f857f27bb4c53488cf",
+    "url": "/dst-recipes/static/media/Eggplant.9374fe09.webp"
+  },
+  {
+    "revision": "d90eb326382289d77d04c152739424b2",
+    "url": "/dst-recipes/static/media/Garlic.d90eb326.webp"
+  },
+  {
+    "revision": "c442a1c499520d7a35ce9b03ac326e46",
+    "url": "/dst-recipes/static/media/Green_Cap.c442a1c4.webp"
+  },
+  {
+    "revision": "3ee5c7bc24a569fefa3fc44d1bd9c4f9",
+    "url": "/dst-recipes/static/media/Hot_Pumpkin.3ee5c7bc.webp"
+  },
+  {
+    "revision": "b30aff439a8bc21ebe58b473a0abdd4b",
+    "url": "/dst-recipes/static/media/Kelp_Fronds.b30aff43.webp"
+  },
+  {
+    "revision": "f98303d9c30d5f5f7b1c0060560157f2",
+    "url": "/dst-recipes/static/media/Lichen.f98303d9.webp"
+  },
+  {
+    "revision": "fff022c59544cc22509b4e7336c6a604",
+    "url": "/dst-recipes/static/media/Mandrake.fff022c5.webp"
+  },
+  {
+    "revision": "a66d391e7668d72ec8e3204327f5e3dd",
+    "url": "/dst-recipes/static/media/Onion.a66d391e.webp"
+  },
+  {
+    "revision": "db6bd99657d6f5f051b2f435f4f62383",
+    "url": "/dst-recipes/static/media/Pepper.db6bd996.webp"
+  },
+  {
+    "revision": "c1477e2d2b0cd58e855fd75a951055c9",
+    "url": "/dst-recipes/static/media/Popcorn.c1477e2d.webp"
+  },
+  {
+    "revision": "573537ecf0d754e52a2b4d22fa95d239",
+    "url": "/dst-recipes/static/media/Potato.573537ec.webp"
+  },
+  {
+    "revision": "2296c776e7ad3ed1bed9e1969641195c",
+    "url": "/dst-recipes/static/media/Pumpkin.2296c776.webp"
+  },
+  {
+    "revision": "ff6b9d56c033c9146047aff4b4f979c8",
+    "url": "/dst-recipes/static/media/Ripe_Stone_Fruit.ff6b9d56.webp"
+  },
+  {
+    "revision": "c3a71224fa0beca081fe5db501b277ed",
+    "url": "/dst-recipes/static/media/Roast_Garlic.c3a71224.webp"
+  },
+  {
+    "revision": "b5735765f04b1019db7fa12e743bd9bc",
+    "url": "/dst-recipes/static/media/Roast_Onion.b5735765.webp"
+  },
+  {
+    "revision": "82c37f4fd0026cfef990791ff6738449",
+    "url": "/dst-recipes/static/media/Roast_Potato.82c37f4f.webp"
+  },
+  {
+    "revision": "874899518e14037f156ce24a32464f8c",
+    "url": "/dst-recipes/static/media/Roast_Toma_Root.87489951.webp"
+  },
+  {
+    "revision": "68d841f4e4d715606d8ca283d2bc3d97",
+    "url": "/dst-recipes/static/media/Roasted_Carrot.68d841f4.webp"
+  },
+  {
+    "revision": "5805ba7bad866020500aab9191388a8b",
+    "url": "/dst-recipes/static/media/Roasted_Pepper.5805ba7b.webp"
+  },
+  {
+    "revision": "79258e3cc5eb38b357e47145f9cf9aeb",
+    "url": "/dst-recipes/static/media/Toma_Root.79258e3c.webp"
+  },
+  {
+    "revision": "7d82d34c036d32aea0221da5312df5d9",
+    "url": "/dst-recipes/static/media/Wagstaff_Portrait.7d82d34c.png"
+  },
+  {
+    "revision": "65b027fc0d62d01853a47439d3f25bcc",
+    "url": "/dst-recipes/static/media/Walani_Portrait.65b027fc.png"
+  },
+  {
+    "revision": "4af3492c03be74a1ee5e7e34581ba9a4",
+    "url": "/dst-recipes/static/media/Walter_Portrait.4af3492c.png"
+  },
+  {
+    "revision": "2eba0c654a6d726b4ddfb94fd67edc35",
+    "url": "/dst-recipes/static/media/Warly_Portrait.2eba0c65.png"
+  },
+  {
+    "revision": "fd367534ae3daaeff710b65e5d279ace",
+    "url": "/dst-recipes/static/media/Wheeler_Portrait.fd367534.png"
+  },
+  {
+    "revision": "dd3c91b52f09ed1e0fd39a83f7869dc9",
+    "url": "/dst-recipes/static/media/Wigfrid_Portrait.dd3c91b5.png"
+  },
+  {
+    "revision": "4adccc3260ff86f50e938b3c2b1afe5d",
+    "url": "/dst-recipes/static/media/Wilba_Portrait.4adccc32.png"
+  },
+  {
+    "revision": "85c82fb14e7543b968a4adac2743ba26",
+    "url": "/dst-recipes/static/media/Wilbur_Portrait.85c82fb1.png"
+  },
+  {
+    "revision": "dc886ac191140a03466a6cc5e755fe26",
+    "url": "/dst-recipes/static/media/Willow_Portrait.dc886ac1.png"
+  },
+  {
+    "revision": "8c2021447b2c4d0ff4a435d28a1816da",
+    "url": "/dst-recipes/static/media/Winona_Portrait.8c202144.png"
+  },
+  {
+    "revision": "b1fa9893932a0388de88be076b7b2375",
+    "url": "/dst-recipes/static/media/Woodlegs_Portrait.b1fa9893.png"
+  },
+  {
+    "revision": "7ad76f66f83292ed17198a1aa63dd1c6",
+    "url": "/dst-recipes/static/media/Wormwood_Portrait.7ad76f66.png"
+  },
+  {
+    "revision": "2617c91fb2cdebcc4080e237bfae389f",
+    "url": "/dst-recipes/static/media/Wortox_Portrait.2617c91f.png"
+  },
+  {
+    "revision": "a7308859a0e7bf29c077d41f83079f46",
+    "url": "/dst-recipes/static/media/any.a7308859.png"
+  },
+  {
+    "revision": "5e996e9d7d7baa4b0b45149e0a1e6626",
+    "url": "/dst-recipes/static/media/batiliskwing.5e996e9d.webp"
+  },
+  {
+    "revision": "2330f11c51c7bef9ad406002c28d5256",
+    "url": "/dst-recipes/static/media/beefalotreats.2330f11c.png"
+  },
+  {
+    "revision": "162426d17b54dc35703762fa0b8bf3bf",
+    "url": "/dst-recipes/static/media/belisa_plumilla-webfont.162426d1.woff2"
+  },
+  {
+    "revision": "8044a113156b6cf60d7c38359b86c943",
+    "url": "/dst-recipes/static/media/belisa_plumilla-webfont.8044a113.woff"
+  },
+  {
+    "revision": "4fdac8f1409501eb90c737315fffccea",
+    "url": "/dst-recipes/static/media/belisa_plumilla.4fdac8f1.ttf"
+  },
+  {
+    "revision": "74dc5a7b467e7a118bc20ef6be14cd3a",
+    "url": "/dst-recipes/static/media/berries.74dc5a7b.webp"
+  },
+  {
+    "revision": "7097feeae51f8142a1a9fdcb018b6229",
+    "url": "/dst-recipes/static/media/boneshards.7097feea.webp"
+  },
+  {
+    "revision": "b11b1bd903013cc0c2a332075a621e39",
+    "url": "/dst-recipes/static/media/breakfastskillet.b11b1bd9.png"
+  },
+  {
+    "revision": "29ccdf76965eaa1a1a037ba976394054",
+    "url": "/dst-recipes/static/media/bunnystew.29ccdf76.png"
+  },
+  {
+    "revision": "7705e3fbb03e7c833a4bfbe969ea5176",
+    "url": "/dst-recipes/static/media/butter.7705e3fb.webp"
+  },
+  {
+    "revision": "74cfaf07cc4af0583dd0024877f392fb",
+    "url": "/dst-recipes/static/media/butterflywings.74cfaf07.webp"
+  },
+  {
+    "revision": "35012bc706bc4fd54c4d4fe0add3306a",
+    "url": "/dst-recipes/static/media/cavebanana.35012bc7.webp"
+  },
+  {
+    "revision": "172808af110d0846b1c73345bd7d8dab",
+    "url": "/dst-recipes/static/media/cookedbatiliskwing.172808af.webp"
+  },
+  {
+    "revision": "e5ddeba0b3928fb818f68919c1d40c50",
+    "url": "/dst-recipes/static/media/cookedcavebanana.e5ddeba0.webp"
+  },
+  {
+    "revision": "8fd0bb4f0a2096524252d315951def0c",
+    "url": "/dst-recipes/static/media/cookedeel.8fd0bb4f.webp"
+  },
+  {
+    "revision": "069080ef0a98f14d17af5b7393ad2ba8",
+    "url": "/dst-recipes/static/media/cookedegg.069080ef.webp"
+  },
+  {
+    "revision": "c2677caee431b31c76fb132535304ade",
+    "url": "/dst-recipes/static/media/cookedfishmorsel.c2677cae.webp"
+  },
+  {
+    "revision": "ee77bce4ea3324f49d3903d97c91d267",
+    "url": "/dst-recipes/static/media/cookedfroglegs.ee77bce4.webp"
+  },
+  {
+    "revision": "ea52c18224a1b6e41291278dd847e8aa",
+    "url": "/dst-recipes/static/media/cookedleafymeat.ea52c182.webp"
+  },
+  {
+    "revision": "b10083c9f27ed9338f58ad2533416243",
+    "url": "/dst-recipes/static/media/cookedmeat.b10083c9.webp"
+  },
+  {
+    "revision": "92e50dbb2d6662d4e6d44204bf07cbfa",
+    "url": "/dst-recipes/static/media/cookedmonstermeat.92e50dbb.webp"
+  },
+  {
+    "revision": "ecc103c2e58fc5093c488a629bc76db4",
+    "url": "/dst-recipes/static/media/cookedmorsel.ecc103c2.webp"
+  },
+  {
+    "revision": "af82e98aacac06b329e285ecf4751a3c",
+    "url": "/dst-recipes/static/media/crockpot.af82e98a.png"
+  },
+  {
+    "revision": "1a1b9543524823a53db7791b31813bde",
+    "url": "/dst-recipes/static/media/deadwobster.1a1b9543.webp"
+  },
+  {
+    "revision": "d296860080bf3eeab5d930acfa270c40",
+    "url": "/dst-recipes/static/media/deliciouswobster.d2968600.webp"
+  },
+  {
+    "revision": "12c6fdcbba6f1a380993fdf95682fe39",
+    "url": "/dst-recipes/static/media/dragonfruit.12c6fdcb.webp"
+  },
+  {
+    "revision": "60f97ba228f90e996b411fda4f44a41c",
+    "url": "/dst-recipes/static/media/drumstick.60f97ba2.webp"
+  },
+  {
+    "revision": "aaa7ea8392d41e028ee3969609846f8b",
+    "url": "/dst-recipes/static/media/durian.aaa7ea83.webp"
+  },
+  {
+    "revision": "572506303ce08a9e5bf36e33074d951f",
+    "url": "/dst-recipes/static/media/eel.57250630.webp"
+  },
+  {
+    "revision": "aa2cb10a7c376a52f588830841f1e464",
+    "url": "/dst-recipes/static/media/egg.aa2cb10a.webp"
+  },
+  {
+    "revision": "e83185b521659c0bc0597b8854d681cc",
+    "url": "/dst-recipes/static/media/electricmilk.e83185b5.webp"
+  },
+  {
+    "revision": "fe5d138d7b0d769e355eace88c736061",
+    "url": "/dst-recipes/static/media/extrasmellydurian.fe5d138d.webp"
+  },
+  {
+    "revision": "d88160d52458bf63bc8fcba086666e29",
+    "url": "/dst-recipes/static/media/fancyspiralledtubers.d88160d5.png"
+  },
+  {
+    "revision": "c73eff0a7bab82228b6ebb0321ad6f5b",
+    "url": "/dst-recipes/static/media/fishes.c73eff0a.png"
+  },
+  {
+    "revision": "506dfadb395324021b69e64523f18805",
+    "url": "/dst-recipes/static/media/fishmeat.506dfadb.webp"
+  },
+  {
+    "revision": "cfd623e6f85a41c433107a74b7cd2513",
+    "url": "/dst-recipes/static/media/fishmorsel.cfd623e6.webp"
+  },
+  {
+    "revision": "c1e85289c82aea625dc0530006003c47",
+    "url": "/dst-recipes/static/media/fishsteak.c1e85289.webp"
+  },
+  {
+    "revision": "76af3e595e1e4145e76b598d1d76a362",
+    "url": "/dst-recipes/static/media/fishtacos.76af3e59.png"
+  },
+  {
+    "revision": "bf5686104187dcc7f090d49987bfb98f",
+    "url": "/dst-recipes/static/media/freshwaterfish.bf568610.webp"
+  },
+  {
+    "revision": "e5f993f673ce919622da73d42bd2b523",
+    "url": "/dst-recipes/static/media/frieddrumstick.e5f993f6.webp"
+  },
+  {
+    "revision": "83318bb1dd309b7d4c42ef1e4cc3771d",
+    "url": "/dst-recipes/static/media/friedtallbirdegg.83318bb1.webp"
+  },
+  {
+    "revision": "df4ce08cffd9f479422fa535a19d2e87",
+    "url": "/dst-recipes/static/media/froglegs.df4ce08c.webp"
+  },
+  {
+    "revision": "9787d5ba2320c48e3f55699f1175e443",
+    "url": "/dst-recipes/static/media/fruits.9787d5ba.png"
+  },
+  {
+    "revision": "814b4412433b51b803ccffd3c250a8d1",
+    "url": "/dst-recipes/static/media/fruitsandveggies.814b4412.png"
+  },
+  {
+    "revision": "9d2532717b448bb00b092eaed59be2c2",
+    "url": "/dst-recipes/static/media/glowberry.9d253271.webp"
+  },
+  {
+    "revision": "c8cbec1a5b20bceddf3d38baca5b3de5",
+    "url": "/dst-recipes/static/media/grilledwatermelon.c8cbec1a.webp"
+  },
+  {
+    "revision": "fb06bef0ef27883d39d5e9b8005292bb",
+    "url": "/dst-recipes/static/media/health.fb06bef0.png"
+  },
+  {
+    "revision": "edb008d37219db79cab12fa340579613",
+    "url": "/dst-recipes/static/media/honey.edb008d3.webp"
+  },
+  {
+    "revision": "8f0948e6b99273f27cbccece15776353",
+    "url": "/dst-recipes/static/media/honeycomb.8f0948e6.webp"
+  },
+  {
+    "revision": "2b986ea87ae7fec6e55cb65dbe815de2",
+    "url": "/dst-recipes/static/media/hunger.2b986ea8.png"
+  },
+  {
+    "revision": "08549535817989f17362e3ed8027d23c",
+    "url": "/dst-recipes/static/media/jerky.08549535.webp"
+  },
+  {
+    "revision": "1accb7e49ed56f6a5617703f1fdf34bf",
+    "url": "/dst-recipes/static/media/juicyberries.1accb7e4.webp"
+  },
+  {
+    "revision": "c5cd6d14ed00eccf9619049492d286a6",
+    "url": "/dst-recipes/static/media/koalefanttrunk.c5cd6d14.webp"
+  },
+  {
+    "revision": "829fe1160bb28eaf4eb3b95cd9ee69aa",
+    "url": "/dst-recipes/static/media/koalefanttrunksteak.829fe116.webp"
+  },
+  {
+    "revision": "06e01fc414ea86b85de57ac14cfc1a01",
+    "url": "/dst-recipes/static/media/leafymeat.06e01fc4.webp"
+  },
+  {
+    "revision": "750b749625b39ca4f683b995800cf0b5",
+    "url": "/dst-recipes/static/media/lesserglowberry.750b7496.webp"
+  },
+  {
+    "revision": "4c7376288aded10c1dac299f1c16f94d",
+    "url": "/dst-recipes/static/media/liveeel.4c737628.webp"
+  },
+  {
+    "revision": "b9b9b1daf30da8a713ad14b24df9052c",
+    "url": "/dst-recipes/static/media/mandrake.b9b9b1da.png"
+  },
+  {
+    "revision": "c993dc98b34f6ea4a7722ea7b66dd9aa",
+    "url": "/dst-recipes/static/media/meat.c993dc98.webp"
+  },
+  {
+    "revision": "b7b5976da00a082e77385b6cabe05bfd",
+    "url": "/dst-recipes/static/media/meats.b7b5976d.png"
+  },
+  {
+    "revision": "b9211e569080adba22db82194d66d5d0",
+    "url": "/dst-recipes/static/media/moleworm.b9211e56.webp"
+  },
+  {
+    "revision": "474fa8ab53bfebe97c1973f361bf8a9f",
+    "url": "/dst-recipes/static/media/monsterfoods.474fa8ab.png"
+  },
+  {
+    "revision": "141a1b74060f5eae2143a0e893eb9c76",
+    "url": "/dst-recipes/static/media/monsterjerky.141a1b74.webp"
+  },
+  {
+    "revision": "5204062a92b9b7b91e3d7fb25635cf2c",
+    "url": "/dst-recipes/static/media/monstermeat.5204062a.webp"
+  },
+  {
+    "revision": "15d8baf360b442e13c1befee27514b8d",
+    "url": "/dst-recipes/static/media/morsel.15d8baf3.webp"
+  },
+  {
+    "revision": "349eb6047086e693e8c8e215e8de8329",
+    "url": "/dst-recipes/static/media/nightmarefuel.349eb604.webp"
+  },
+  {
+    "revision": "2061e8e583eb974d6db1f662dec51355",
+    "url": "/dst-recipes/static/media/plainomelette.2061e8e5.png"
+  },
+  {
+    "revision": "405c154802bcb9b8237fff8bbf28cc5c",
+    "url": "/dst-recipes/static/media/pomegranate.405c1548.webp"
+  },
+  {
+    "revision": "38c7d593f09992b341c776426b9bc5c5",
+    "url": "/dst-recipes/static/media/prepareddragonfruit.38c7d593.webp"
+  },
+  {
+    "revision": "7a889a384d34b97d171315dfd4f7d6c8",
+    "url": "/dst-recipes/static/media/rawfish.7a889a38.webp"
+  },
+  {
+    "revision": "37b5486efee412cd5628559a16037d60",
+    "url": "/dst-recipes/static/media/redcap.37b5486e.webp"
+  },
+  {
+    "revision": "221e54c666746d946a3d566cf439897b",
+    "url": "/dst-recipes/static/media/roastedberries.221e54c6.webp"
+  },
+  {
+    "revision": "ee8933de75216aa1210b7cf77ec5015d",
+    "url": "/dst-recipes/static/media/roastedbirchnut.ee8933de.webp"
+  },
+  {
+    "revision": "f5ccc49f21531334cd11b412512ef8a7",
+    "url": "/dst-recipes/static/media/roastedjuicyberries.f5ccc49f.webp"
+  },
+  {
+    "revision": "7d819d6f771e98d5ce1012bca0952251",
+    "url": "/dst-recipes/static/media/royaljelly.7d819d6f.png"
+  },
+  {
+    "revision": "b44c2c2a6a690517d6bdf3bbba44254a",
+    "url": "/dst-recipes/static/media/royaljelly.b44c2c2a.webp"
+  },
+  {
+    "revision": "52b1f323d8caec69cdfd780949afac98",
+    "url": "/dst-recipes/static/media/sanity.52b1f323.png"
+  },
+  {
+    "revision": "ee7ef4e5058e74d92f5b7483653227d3",
+    "url": "/dst-recipes/static/media/slicedpomegranate.ee7ef4e5.webp"
+  },
+  {
+    "revision": "1e4601edd8275b98d85eaf47dd619886",
+    "url": "/dst-recipes/static/media/smalljerky.1e4601ed.webp"
+  },
+  {
+    "revision": "00ed956228c0cb3bcabca16f9e6f5c2d",
+    "url": "/dst-recipes/static/media/steamedtwigs.00ed9562.png"
+  },
+  {
+    "revision": "d9b24a3ce0705796d3aeffa1320db40f",
+    "url": "/dst-recipes/static/media/stonefruit.d9b24a3c.webp"
+  },
+  {
+    "revision": "fd8b30dccf9ea77e774d27bf99a90c4a",
+    "url": "/dst-recipes/static/media/tallbirdegg.fd8b30dc.webp"
+  },
+  {
+    "revision": "0bb7f98f2a850cb554f701172a16af27",
+    "url": "/dst-recipes/static/media/tallscotcheggs.0bb7f98f.png"
+  },
+  {
+    "revision": "b6d87d2ec7c699dca3ed972318b8b753",
+    "url": "/dst-recipes/static/media/twigs.b6d87d2e.webp"
+  },
+  {
+    "revision": "7e4ab8c242a1c46af7c0f3af09d360b6",
+    "url": "/dst-recipes/static/media/vegetables.7e4ab8c2.png"
+  },
+  {
+    "revision": "2688be435d7189218b7751cfe0150f84",
+    "url": "/dst-recipes/static/media/voltgoathorn.2688be43.webp"
+  },
+  {
+    "revision": "9bdc29da52b82bece4c3d684aae0cf0e",
+    "url": "/dst-recipes/static/media/watermelon.9bdc29da.webp"
+  },
+  {
+    "revision": "90884411bc2f06cc3c4830905328dbb7",
+    "url": "/dst-recipes/static/media/winterkoalefanttrunk.90884411.webp"
+  },
+  {
+    "revision": "744f9e3795cb258da17254d52300f063",
+    "url": "/dst-recipes/static/media/wobster.744f9e37.webp"
+  }
+]);
